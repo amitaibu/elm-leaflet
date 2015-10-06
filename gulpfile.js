453c2db4fbd2ac2d0c5fcdf3993aa70786b93f02
Amitai Burstein <amitai@gizra.com>
2015-10-06 20:03:44 +0300

    Fix gulp errors

--- gulpfile.js
+++ gulpfile.js
@@ -188,10 +188,10 @@ gulp.task("serve:prod", function () {
 });
 
 // Default task, run when just writing "gulp" in the terminal
-gulp.task("default", ["clean:dev", "serve:dev", "watch"]);
+gulp.task("default", ["build", "serve:dev", "watch"]);
 
 // Builds the site but doesnt serve it to you
-gulp.task("build", ["clean:dev", "copy:dev", "elm", "styles"], function () {});
+gulp.task("build", ["clean:dev", "styles", "copy:dev", "elm"], function () {});
 
 // Builds your site with the "build" command and then runs all the optimizations on
 // it and outputs it to "./dist"
